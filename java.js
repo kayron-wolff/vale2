0fc401ea4af4816171b3e1fedd3b3cc9bfdafaeb
KayronOffline <gustavo.asus3@outlook.com>
2025-03-28 12:47:14 -0300

    Adião do Java.js

---
+++ java.js
@@ -0,0 +1,11 @@
+let titulo = "Vale do Anahangabaú: História do Local";
+        let indice = 0;
+
+        function rolarTitulo() {
+            // Define o título exibido como uma parte do texto original
+            document.title = titulo.substring(indice) + " " + titulo.substring(0, indice);
+            indice = (indice + 1) % titulo.length; // Avança o índice, reiniciando ao final
+        }
+
+        // Atualiza o título a cada intervalo de tempo
+        setInterval(rolarTitulo, 200); 
